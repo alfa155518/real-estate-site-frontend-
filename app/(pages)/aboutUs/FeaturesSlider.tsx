38a3b3b435b9f5b18d2b25cbcbc06743f82d958d
Ahmed Hassop <Ahmed Hassob>
2025-10-16 13:06:51 +0300

    create: create slides in about-us page and connect with api

--- app/(pages)/aboutUs/FeaturesSlider.tsx
+++ app/(pages)/aboutUs/FeaturesSlider.tsx
@@ -1,49 +1,50 @@
-'use client';
+"use client";
 
-
-import 'swiper/css';
-import 'swiper/css/pagination';
+import "swiper/css";
+import "swiper/css/pagination";
 
 // Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
-
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // import required modules
-import { Pagination, Autoplay } from 'swiper/modules';
-import Image from 'next/image';
-import styles from '@/sass/pages/about-us/features-slider.module.scss';
-import SectionName from '@/components/common/SectionName';
-
-const featureImages:string[] = [
-    "/images/about-us/properties-for-sale-1.webp",
-    "/images/about-us/properties-for-sale-2.webp",
-    "/images/about-us/properties-for-sale-3.webp",
-    "/images/about-us/apartment-for-sale-4.webp",
-    "/images/about-us/apartment-for-sale-5.webp",
-    "/images/about-us/apartment-for-sale-6.webp",
-    "/images/about-us/apartment-for-sale-7.webp",
-    "/images/about-us/apartment-for-sale-8.webp",
-    "/images/about-us/apartment-for-sale-9.webp",
-]
+import { Pagination, Autoplay } from "swiper/modules";
+import Image from "next/image";
+import styles from "@/sass/pages/about-us/features-slider.module.scss";
+import SectionName from "@/components/common/SectionName";
+import { useEffect } from "react";
+import useSlidersStore from "@/store/SlidersStore";
 
 export default function FeaturesSlider() {
-     const pagination = {
+  const { handleGetSlider, sliders, isLoading, defaultSlide } =
+    useSlidersStore();
+
+  useEffect(() => {
+    async function fetchData() {
+      await handleGetSlider(3);
+    }
+    fetchData();
+  }, [handleGetSlider]);
+
+  const pagination = {
     clickable: true,
     renderBullet: function (index: number, className: string) {
       return `<span class="${className}">${index + 1}</span>`;
     },
   };
-return (
-<section className={styles.featuresSlider}>
-              <SectionName title="
+  return (
+    <section className={styles.featuresSlider}>
+      <SectionName
+        title="
               اروع العقارات
-              " subtitle="
+              "
+        subtitle="
               اكتشف اروع العقارات المتاحة الآن
-              "/>
+              "
+      />
 
-          <Swiper
+      <Swiper
         pagination={pagination}
-        dir='rtl'
+        dir="rtl"
         modules={[Pagination, Autoplay]}
         loop={true}
         autoplay={{
@@ -53,14 +54,36 @@ return (
         grabCursor={true}
       >
         <div className="swiper-pagination" />
-            {
-            featureImages.map((featureImage) => (
-                <SwiperSlide key={featureImage} className={styles.swiperSlide}>
-                    <Image src={featureImage} alt={featureImage} width={300} height={300} />
-                </SwiperSlide>
-            ))
-        }
+        {isLoading || sliders.id === 0 ? (
+          <SwiperSlide className={styles.swiperSlide}>
+            <Image
+              src={defaultSlide}
+              alt={`default_slide`}
+              width={1920}
+              height={1080}
+              sizes="(max-width: 768px) 100vw,
+            (max-width: 1200px) 100vw,
+            100vw"
+              priority={true}
+            />
+          </SwiperSlide>
+        ) : (
+          sliders.images.map((image: string, index: number) => (
+            <SwiperSlide key={index} className={styles.swiperSlide}>
+              <Image
+                src={image}
+                alt={`slide-${index + 1}`}
+                width={300}
+                height={300}
+                sizes="(max-width: 768px) 100vw,
+                (max-width: 1200px) 100vw,
+                100vw"
+                priority={index === 0}
+              />
+            </SwiperSlide>
+          ))
+        )}
       </Swiper>
-</section>
-)
+    </section>
+  );
 }
